87a7a7031f2ef9fd2ddd0dc7360b29eb4f52b7b1
Steb <steb.vs@outlook.com>
2019-11-09 16:40:34 +0100

    player

---
+++ PoubelleLaVie/Assets/Scripts/Input/InputHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class InputHelper
+{
+    /// <summary>
+    /// X axis.
+    /// </summary>
+    public const string HORIZONTAL = "Horizontal";
+
+    /// <summary>
+    /// Y axis.
+    /// </summary>
+    public const string VERTICAL = "Vertical";
+
+    /// <summary>
+    /// Take or drop.
+    /// </summary>
+    public const string TAKE_N_DROP = "Take / Drop";
+
+    /// <summary>
+    /// Use.
+    /// </summary>
+    public const string USE = "Use";
+}
