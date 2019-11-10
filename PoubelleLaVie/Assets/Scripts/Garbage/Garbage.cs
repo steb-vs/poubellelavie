8a708aefe9cdda96b5a37a8ec3410d5bbb68de0f
Thomas <thomas.epitech@gmail.com>
2019-11-10 12:18:38 +0100

    Random garbage

--- PoubelleLaVie/Assets/Scripts/Garbage/Garbage.cs
+++ PoubelleLaVie/Assets/Scripts/Garbage/Garbage.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,4 +7,10 @@ public class Garbage : MonoBehaviour
 {
     public WorldTile worldTile;
     public Sprite[] sprites;
+
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[UnityEngine.Random.Range(0, sprites.Length - 1)];
+    }
 }
